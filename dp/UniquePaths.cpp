95fe1dc6e5e7d8bc5e023d1d3b6ec95f0ab63bd6
Trilasha Mazumder <tilupm2020@gmail.com>
2022-11-08 19:13:21 +0530

    addition

---
+++ dp/UniquePaths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // int factorial(int n)
+    // {
+    //     int ans=1;
+    //     for(int i=n;i>=1;--i)
+    //     ans*=i;
+    //     return ans;
+    // }
+    
+    //both long long & double are being accepted
+    //even changed the return type to ll and double --> it got accepted
+    //however factorial call as such individually throws runtime error as it doesn't fit into    long long data type also
+        long long uniquePaths(int m, int n) {
+        int var=(m-1)+(n-1);
+        int max_one=max(m-1,n-1);
+        int min_one=min(m-1,n-1);
+        long long  pre=1;
+        int y=1;
+        for(int x=m+n-2;x>max_one;--x)
+        {
+            if(y==(min_one+1))
+            break;
+            pre=(pre*x)/y++;
+
+        }
+        return pre;
+    }
+};
